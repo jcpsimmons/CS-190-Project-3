cd22a9aacf48362ee2d9b23c835a3e51d2e98326
Matthew Bobke <mcbobke@gmail.com>
2017-05-28 20:49:08 -0700

    Transparent wall activation created
    Press X to make the walls completely transparent for 5 seconds. Once 5 seconds pass, the walls reset. This can be done 5 times. According to Unity documentation, I used a legacy shader for this process, but the new way is to create a transparent material using the Standard Shader (https://docs.unity3d.com/Manual/shader-StandardShader.html). If we think this might make a difference, we can re-implement using the transparent material that I created in the Assets/Materials folder.

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -2,21 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     public float speed = 5f;
     private Vector3 moveDirection;
+    private float shaderTimer = 0.0f;
+    public float shaderTimerReset = 5.0f;
+    public int limit = 5;
+    private Shader standardShader;
+    private Shader transparentShader;
+    private Color standardColor;
+    private Color transparentColor;
+    private bool wallsTransformed = false;
+
+    void Start()
+    {
+        standardShader = Shader.Find("Standard");
+        transparentShader = Shader.Find("Transparent/Diffuse");
+        standardColor = GameObject.Find("LeftWall").GetComponent<MeshRenderer>().material.color;
+        transparentColor = new Color(0.0f, 0.0f, 0.0f, 0.0f); // Edit the last value here to change how transparent the walls become.
+    }
 
     void Update()
     {
-        float horizontal = speed * Input.GetAxis("Horizontal");
-        float vertical = speed * Input.GetAxis("Vertical");
+        float horizontal = speed*Input.GetAxis("Horizontal");
+        float vertical = speed*Input.GetAxis("Vertical");
 
-        moveDirection = (horizontal * this.transform.right) + (vertical * this.transform.forward);
+        moveDirection = (horizontal*this.transform.right) + (vertical*this.transform.forward);
+
+        if (Input.GetKeyDown(KeyCode.X) && limit > 0 && shaderTimer <= 0.0f)
+        {
+            TransformWalls();
+            wallsTransformed = true;
+            limit--;
+            shaderTimer = shaderTimerReset;
+        }
+
+        if (shaderTimer <= 0.0f && wallsTransformed)
+        {
+            RevertWalls();
+            wallsTransformed = false;
+        }
+
+        if (shaderTimer > 0.0f)
+        {
+            shaderTimer -= Time.deltaTime;
+        }
     }
 
     void FixedUpdate()
     {
-        this.transform.Translate(moveDirection * Time.deltaTime, Space.World);
+        this.transform.Translate(moveDirection*Time.deltaTime, Space.World);
+    }
+
+    private void TransformWalls()
+    {
+        foreach (var gameObj in FindObjectsOfType(typeof (GameObject)) as GameObject[])
+        {
+            if (gameObj.name.Contains("Wall") && gameObj.activeSelf)
+            {
+                gameObj.GetComponent<MeshRenderer>().material.shader = transparentShader;
+                gameObj.GetComponent<MeshRenderer>().material.color = transparentColor;
+            }
+        }
+    }
+
+    private void RevertWalls()
+    {
+        foreach (var gameObj in FindObjectsOfType(typeof(GameObject)) as GameObject[])
+        {
+            if (gameObj.name.Contains("Wall") && gameObj.activeSelf)
+            {
+                gameObj.GetComponent<MeshRenderer>().material.shader = standardShader;
+                gameObj.GetComponent<MeshRenderer>().material.color = standardColor;
+            }
+        }
     }
 }
