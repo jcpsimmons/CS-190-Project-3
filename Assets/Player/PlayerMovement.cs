087479d52c9275c2402a87cf225f08f08bee93d6
Edwin Ho <eaho555@gmail.com>
2017-06-01 16:16:05 -0700

    Footsteps + higher alpha + fireworks added

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -7,9 +7,14 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 5f;
     private Vector3 moveDirection;
+
+    public float footstepInterval = .5f;
+    private float footstepCountdown = 0f;
+
     private float shaderTimer = 0.0f;
     public float shaderTimerReset = 5.0f;
     public int limit = 5;
+    public ParticleSystem fireworks;
     private Shader standardShader;
     private Shader transparentShader;
     private Color standardColor;
@@ -21,7 +26,7 @@ public class PlayerMovement : MonoBehaviour
         standardShader = Shader.Find("Standard");
         transparentShader = Shader.Find("Transparent/Diffuse");
         standardColor = GameObject.Find("LeftWall").GetComponent<MeshRenderer>().material.color;
-        transparentColor = new Color(0.0f, 0.0f, 0.0f, 0.0f); // Edit the last value here to change how transparent the walls become.
+        transparentColor = new Color(0.0f, 0.0f, 0.0f, 0.05f); // Edit the last value here to change how transparent the walls become.
     }
 
     void Update()
@@ -30,6 +35,14 @@ public class PlayerMovement : MonoBehaviour
         float vertical = speed*Input.GetAxis("Vertical");
 
         moveDirection = (horizontal*this.transform.right) + (vertical*this.transform.forward);
+
+        footstepCountdown -= Time.deltaTime;
+        if (moveDirection != Vector3.zero && footstepCountdown <= 0f)
+        {
+            Debug.Log("Playing Footsteps");
+            AkSoundEngine.PostEvent("Footstep_Randomizer", this.gameObject);
+            footstepCountdown = footstepInterval;
+        }
 
         if (Input.GetKeyDown(KeyCode.X) && limit > 0 && shaderTimer <= 0.0f)
         {
@@ -58,6 +71,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void TransformWalls()
     {
+        ToggleFireworks(true);
         foreach (var gameObj in FindObjectsOfType(typeof (GameObject)) as GameObject[])
         {
             if (gameObj.name.Contains("Wall") && gameObj.activeSelf)
@@ -70,6 +84,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void RevertWalls()
     {
+        ToggleFireworks(false);
         foreach (var gameObj in FindObjectsOfType(typeof(GameObject)) as GameObject[])
         {
             if (gameObj.name.Contains("Wall") && gameObj.activeSelf)
@@ -77,6 +92,18 @@ public class PlayerMovement : MonoBehaviour
                 gameObj.GetComponent<MeshRenderer>().material.shader = standardShader;
                 gameObj.GetComponent<MeshRenderer>().material.color = standardColor;
             }
+        }
+    }
+
+    void ToggleFireworks(bool active)
+    {
+        if (active)
+        {
+            fireworks.Play();
+        }
+        else
+        {
+            fireworks.Stop();
         }
     }
 }
